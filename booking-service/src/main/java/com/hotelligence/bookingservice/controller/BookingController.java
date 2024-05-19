dcbfb310d140f60d01cf91a8fefbffe025cb9c0d
Le Doan Tan Tri <ledoantantri@gmail.com>
2024-05-20 03:45:57 +0700

    adjust api url

--- booking-service/src/main/java/com/hotelligence/bookingservice/controller/BookingController.java
+++ booking-service/src/main/java/com/hotelligence/bookingservice/controller/BookingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/booking")
+@RequestMapping("/api/bookings")
 @RequiredArgsConstructor
 public class BookingController {
 
