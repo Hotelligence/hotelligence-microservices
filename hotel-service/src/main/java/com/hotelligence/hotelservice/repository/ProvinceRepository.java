bc99dab4cf85e22907ee61d752a28ee49f496cd2
Le Doan Tan Tri <ledoantantri@gmail.com>
2024-12-04 15:12:27 +0700

    remove Province (unnecessary)

--- hotel-service/src/main/java/com/hotelligence/hotelservice/repository/ProvinceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotelligence.hotelservice.repository;
-
-import com.hotelligence.hotelservice.model.Province;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProvinceRepository extends MongoRepository<Province, String> {
-}
